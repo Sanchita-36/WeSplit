b4827d728ac4625463dfd2af6dae2e5956ce25e9
Sanchita-36 <sanchitalachke@gmail.com>
2024-12-03 14:35:45 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by sanchita lachke on 03/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
